d2e5955d30fbbf0f49f125a771ad3d141b38af75
felociouske <felociouske@gmail.com>
2026-04-19 10:01:24 +0300

    Updated the sidebar.jsx fetching

--- src/components/layout/PageWrapper.jsx
+++ src/components/layout/PageWrapper.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SideBar";
 
 export default function PageWrapper({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
